47f3a60d5c6e7e057af5060f4c8c7aa4c5abd797
Adam Johnson <adam@pkqk.net>
2010-09-25 16:54:09 +0100

    instatube

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instatube::Application.config.secret_token = '28274f78c928b3ac049e85b0d762abe48b9f1fa846bdc38a65093deaa7eb8f7cf5775902d92317dd00f50258fb9be43bd29f11a583c04a6db2458e23ad6c5c25'
