5f348b6ddd16619326c4f5ac621a71560e253442
Adam Johnson <adam@pkqk.net>
2010-09-27 01:17:46 +0100

    show errors nicer

--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -1,10 +1,14 @@
 class FormBuilder < ActionView::Helpers::FormBuilder
   def text_field(method, options={})
-    error = @object.errors.on(method)
+    error = @object.errors.on(method) if @object
     classes = []
     classes << 'error' if error
     @template.content_tag(:p, :class => classes.join(' ')) do
-      label(method) + super + (error ? @template.content_tag(:span, error, :class => 'message') : '')
+      [
+        label(method),
+        super,
+        (@template.content_tag(:span, error, :class => 'message') if error)
+      ].compact.inject(&:+)
     end
   end
 end
